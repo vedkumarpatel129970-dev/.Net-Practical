885eb475e7ea85147bc9f914944e8e7834bb1f43
vedkumarpatel129970-dev <vedkumar.patel129970@marwadiuniversity.ac.in>
2026-08-01 18:33:52 +0530

    Add files via upload

---
+++ Practical-3/Expense Tracking Module/Expense Tracking Module/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n;
+        double amount, total = 0.0;
+        string place, mode;
+
+        try
+        {
+            Console.Write("How many expenses do you want to add? ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("\nExpense " + i);
+
+                Console.Write("Where did you spend? ");
+                place = Console.ReadLine();
+
+                Console.Write("Enter Expense Amount: ");
+                amount = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter Payment Mode (Cash/Card/UPI): ");
+                mode = Console.ReadLine();
+
+                total = total + amount;
+
+                Console.WriteLine("Expense Added Successfully!");
+            }
+
+            Console.WriteLine("\nTotal Expense = Rs." + total);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid Input! Please enter valid numbers.");
+        }
+
+        Console.ReadLine();
+    }
+}
